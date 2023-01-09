e95cd3b33062b88401e2e8670fb1b259058dda71
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-09 21:31:44 +0000

    Create posts route

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 dotenv.config();
 
 mongoose.set('strictQuery', false);
@@ -26,6 +27,7 @@ app.use(morgan('common'));
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
 
 app.get('/', (req, res) => res.send('Welcome to home page'));
 app.get('/users', (req, res) => res.send('Welcome to users page'));
---
+++ api/routes/posts.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => console.log('posts page'));
+
+module.exports = router;
