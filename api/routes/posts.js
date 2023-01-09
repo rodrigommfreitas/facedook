d994f51f3a6b0446227dc5c394b0679fc6937670
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-09 23:25:53 +0000

    Get single post and get feed posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 router.get('/', (req, res) => console.log('posts page'));
 
@@ -64,8 +65,31 @@ router.put('/:id/like', async (req, res) => {
   }
 });
 
-// Get feed posts
+// Get post
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// Get posts from user
+// Get feed posts (posts from user and his friends)
+router.get('/feed/all', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const currentUserPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+
+    res.status(200).json(currentUserPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
