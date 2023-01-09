dd7a3cf887b948a59765c19b0d28cc0d7d414c55
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-09 21:46:40 +0000

    Create post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,29 @@
 const router = require('express').Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => console.log('posts page'));
+
+// Create post
+router.post('/new', async (req, res) => {
+  const newPost = new Post(req.body);
+
+  try {
+    const savedPost = await newPost.save();
+
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update post
+
+// Delete post
+
+// Like post
+
+// Get feed posts
+
+// Get posts from user
 
 module.exports = router;
