89627068013a5551834852ffb52b69a1b70d70b0
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-09 22:46:45 +0000

    Like post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -49,6 +49,20 @@ router.delete('/:id/delete', async (req, res) => {
 });
 
 // Like post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json('The post has been liked.');
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json('Like has been removed from the post.');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Get feed posts
 
