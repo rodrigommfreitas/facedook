5c0ab6a325f998e79de19c08071082c30c4b884e
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-09 23:59:07 +0000

    Initial front-end commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className='h-screen w-screen bg-blue-500'>
+      <h1 className='bg-red-500 w-fit absolute left-1/2 top-1/2'>
+        Hello World
+      </h1>
+    </div>
+  );
+};
