f1f7df72abda87e8fceabba7645043523957ef61
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-10 00:44:15 +0000

    Create tailwind custom colors

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'light-gray': '#F0F2F5',
+      'dark-gray': '#65676B',
+      blue: '#1B74E4',
+    },
     extend: {},
   },
   plugins: [],
