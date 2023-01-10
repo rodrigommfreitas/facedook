ff65ebc223d71371cb2d267c28aaf7b96846f2ba
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-10 00:01:37 +0000

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,3 @@
 export const App = () => {
-  return (
-    <div className='h-screen w-screen bg-blue-500'>
-      <h1 className='bg-red-500 w-fit absolute left-1/2 top-1/2'>
-        Hello World
-      </h1>
-    </div>
-  );
+  return <div className='h-screen w-screen bg-blue-500'></div>;
 };
