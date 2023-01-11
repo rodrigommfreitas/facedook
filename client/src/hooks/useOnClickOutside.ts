1027a85f72072146c538b2b7ab6e1871255cf0b1
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-11 11:50:39 +0000

    Add useOnClickOutside custom hook

---
+++ client/src/hooks/useOnClickOutside.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+export const useOnClickOutside = (
+  ref: any,
+  handler: (event: Event) => void
+) => {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
