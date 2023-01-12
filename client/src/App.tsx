169d182c2cee351a88c9a74f2e87a1e56dbb1209
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-12 23:10:35 +0000

    Implement react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Profile } from './pages/Profile';
+import { Register } from './pages/Register';
 
 export const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path='*' element={<Home />}></Route>
+      <Route index element={<Home />}></Route>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/home' element={<Home />}></Route>
+
+      <Route path='/profile/:username' element={<Profile />}></Route>
+
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/register' element={<Register />}></Route>
+    </Routes>
+  );
 };
