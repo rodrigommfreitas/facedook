0eb644251638bc6e91c3b272483c20dc37cc37ec
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-12 22:47:08 +0000

    Remove overflow-x

--- client/src/components/Rightbar/Rightbar.tsx
+++ client/src/components/Rightbar/Rightbar.tsx
@@ -8,7 +8,7 @@ export const Rightbar = () => {
     'w-fit h-fit rounded-full p-1 hover:bg-gray-200 active:bg-gray-300 active:scale-95 transition-all';
 
   return (
-    <div className='w-1/4 lg:w-[200px] lg:max-w-[290px] 2xl:max-w-[360px] xl:w-[360px] max-w-[360px] hidden md:block fixed right-2'>
+    <div className='w-1/4 lg:w-[200px] lg:max-w-[290px] 2xl:max-w-[360px] xl:w-[360px] max-w-[360px] hidden md:block fixed right-2 overflow-y-scroll overflow-x-hidden'>
       <div className='text-gray-dark w-full flex justify-between items-center'>
         <h1 className='ml-4 font-bold text-lg'>Contacts</h1>
         <div className='flex mr-2 gap-1 xl:gap-4'>
