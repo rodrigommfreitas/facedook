7021cf7f7634164f75c8cf042b3226d445a9cff0
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-12 20:59:07 +0000

    Add z index

--- client/src/components/Topbar/Topbar.tsx
+++ client/src/components/Topbar/Topbar.tsx
@@ -4,7 +4,7 @@ import { Right } from './Right';
 
 export const Topbar = () => {
   return (
-    <div className='bg-white sticky top-0 h-14 w-screen px-4 flex md:justify-between shadow-md shadow-gray-200'>
+    <div className='bg-white sticky top-0 h-14 w-screen px-4 flex md:justify-between shadow-md shadow-gray-200 z-10'>
       <Left />
       <Nav />
       <Right />
