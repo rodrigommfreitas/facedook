b26b6a6182e87f2af2770186f79c1baeca6481e4
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-12 22:46:45 +0000

    Place contacts inside list items

--- client/src/components/Rightbar/Contacts.tsx
+++ client/src/components/Rightbar/Contacts.tsx
@@ -2,13 +2,25 @@ import { Contact } from './Contact';
 
 export const Contacts = () => {
   return (
-    <ul className='w-full pt-2 ml-2'>
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
+    <ul>
+      <li>
+        <Contact />
+      </li>
+      <li>
+        <Contact />
+      </li>
+      <li>
+        <Contact />
+      </li>
+      <li>
+        <Contact />
+      </li>
+      <li>
+        <Contact />
+      </li>
+      <li>
+        <Contact />
+      </li>
     </ul>
   );
 };
