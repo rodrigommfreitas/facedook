d8646b85e51c469ea9c931c563da9b540cb3c4e5
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-13 00:54:57 +0000

    Fetch posts data and render

--- client/src/components/Feed/Feed.tsx
+++ client/src/components/Feed/Feed.tsx
@@ -1,11 +1,25 @@
+import { instance as axios } from '../../globals/axios';
+import { useEffect, useState } from 'react';
 import { Post } from '../Post/Post';
 import { Share } from '../Share/Share';
 
 export const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('posts/feed/63bb62f4a18c155f79c00549');
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className='flex flex-col gap-4 h-screen w-[640px] md:w-2/3 lg:w-[572px] xl:w-[652px] sm:mx-auto md:mx-8 lg:mx-auto'>
       <Share />
-      <Post />
+      {posts.map((post, i) => (
+        <Post key={i} post={post} />
+      ))}
     </div>
   );
 };
