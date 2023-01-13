bb15a364a9ab9ffdb5520f122ed95365c57d79fe
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-13 15:54:53 +0000

    Define User and Post types

--- client/src/globals/types.ts
+++ client/src/globals/types.ts
@@ -3,3 +3,24 @@ export type IconProps = {
   height?: string;
   fill?: string;
 };
+
+export type UserType = {
+  _id: string;
+  username: string;
+  email: string;
+  password: string;
+  profilePicture: string;
+  bannerPicture: string;
+  followers: string[];
+  following: string[];
+  isAdmin: boolean;
+};
+
+export type PostType = {
+  _id: string;
+  userId: string;
+  desc: string;
+  img: string;
+  likes: string[];
+  createdAt: Date;
+};
