d4fa8cecb6bdace0a6473041903dec849718f3fa
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-13 19:02:37 +0000

    Get single user posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -90,4 +90,15 @@ router.get('/feed/:userId', async (req, res) => {
   }
 });
 
+// Get user posts
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
