ba7043242ce536bdf5d1b22df7af2607953087f4
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-13 19:03:47 +0000

    Adjust styling between Home and Profile feed

--- client/src/components/Feed/Feed.tsx
+++ client/src/components/Feed/Feed.tsx
@@ -4,19 +4,30 @@ import { Post } from '../Post/Post';
 import { Share } from '../Share/Share';
 import { PostType } from '../../globals/types';
 
-export const Feed = () => {
+type Props = {
+  username?: string;
+};
+
+export const Feed = ({ username }: Props) => {
   const [posts, setPosts] = useState<PostType[]>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get('posts/feed/63bb62f4a18c155f79c00549');
+      const res = username
+        ? await axios.get('posts/profile/' + username)
+        : await axios.get('posts/feed/63bb62f4a18c155f79c00549');
       setPosts(res.data);
     };
     fetchPosts();
   }, []);
 
   return (
-    <div className='flex flex-col gap-4 h-screen w-[640px] md:w-2/3 lg:w-[572px] xl:w-[652px] sm:mx-auto md:mx-8 lg:mx-auto'>
+    <div
+      className={`flex flex-col gap-4 ${
+        !username &&
+        'h-screen w-[640px] md:w-2/3 lg:w-[572px] xl:w-[652px] sm:mx-auto md:mx-8 lg:mx-auto'
+      } `}
+    >
       <Share />
       {posts.map((post, i) => (
         <Post key={i} post={post} />
