8f4ae70b8456422c3530d1de3cfd04759eff6967
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 23:10:38 +0000

    Show user friends

--- client/src/components/Rightbar/Contacts.tsx
+++ client/src/components/Rightbar/Contacts.tsx
@@ -1,26 +1,30 @@
+import { useContext, useEffect, useState } from 'react';
+import { instance as axios } from '../../globals/axios';
+import { AuthContext } from '../../context/AuthContext';
+import { UserType } from '../../globals/types';
 import { Contact } from './Contact';
 
 export const Contacts = () => {
+  const { user } = useContext(AuthContext);
+  const [friends, setFriends] = useState<UserType[]>([]);
+
+  useEffect(() => {
+    const getFriends = async () => {
+      try {
+        const friendList = await axios.get('/users/friends/' + user?._id);
+        setFriends(friendList.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFriends();
+  }, []);
+
   return (
     <ul className='mt-2 pl-2'>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
+      {friends.map((friend: UserType) => (
+        <Contact key={friend._id} user={friend} />
+      ))}
     </ul>
   );
 };
