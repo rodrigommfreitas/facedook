b551345313dc83968e7e39b2616a334d27190096
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 23:11:55 +0000

    Show user info and nav link to profile

--- client/src/components/Rightbar/Contact.tsx
+++ client/src/components/Rightbar/Contact.tsx
@@ -1,12 +1,28 @@
-export const Contact = () => {
+import { useNavigate } from 'react-router-dom';
+import { UserType } from '../../globals/types';
+
+type Props = {
+  user: UserType;
+};
+
+export const Contact = ({ user }: Props) => {
+  const navigate = useNavigate();
   return (
-    <button className='w-full p-2 flex gap-2 items-center hover:bg-gray-200 active:bg-gray-300 transition-all rounded-lg'>
-      <img
-        src=''
-        alt=''
-        className='bg-primary w-9 h-9 rounded-full object-cover'
-      />
-      <span>Chuck Norris</span>
-    </button>
+    <li onClick={() => navigate('/profile/' + user.username)}>
+      <button className='w-full p-2 flex gap-2 items-center hover:bg-gray-200 active:bg-gray-300 transition-all rounded-lg'>
+        {user?.profilePicture !== '' ? (
+          <img
+            src={user?.profilePicture}
+            alt=''
+            className='w-9 h-9 rounded-full object-cover'
+          />
+        ) : (
+          <div className='text-3xl font-black text-primary w-9 h-9 rounded-full bg-gray-light object-cover flex items-center justify-center'>
+            {user.username.charAt(0).toLocaleUpperCase()}
+          </div>
+        )}
+        <span>{user.username}</span>
+      </button>
+    </li>
   );
 };
