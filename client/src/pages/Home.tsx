7974d6b46b3be91fb83250f8a8b15bebd115ea5d
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 18:27:34 +0000

    Redirect to home URL on page load (useful after login)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Feed } from '../components/Feed/Feed';
 import { Leftbar } from '../components/Leftbar/Leftbar';
 import { Rightbar } from '../components/Rightbar/Rightbar';
 import { Topbar } from '../components/Topbar/Topbar';
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/home');
+  }, []);
+
   return (
     <div className='bg-gray-light w-full h-fit'>
       <Topbar />
