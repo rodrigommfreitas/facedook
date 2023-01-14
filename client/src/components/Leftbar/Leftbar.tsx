7dc6f333d05c77aedbb53c9ae675c82c5e547f62
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 23:14:12 +0000

    Show two users as suggested

--- client/src/components/Leftbar/Leftbar.tsx
+++ client/src/components/Leftbar/Leftbar.tsx
@@ -1,6 +1,8 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { instance as axios } from '../../globals/axios';
 import { AuthContext } from '../../context/AuthContext';
+import { UserType } from '../../globals/types';
 import { ArrowDownIcon } from '../Icons/ArrowDownIcon';
 import { FriendsIcon } from '../Icons/FriendsIcon';
 import { GroupsFillIcon } from '../Icons/GroupsFillIcon';
@@ -9,79 +11,142 @@ import { PlayFillIcon } from '../Icons/PlayFillIcon';
 
 export const Leftbar = () => {
   const { user } = useContext(AuthContext);
-
+  const [jane, setJane] = useState<UserType | Record<string, never>>({});
+  const [derek, setDerek] = useState<UserType | Record<string, never>>({});
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const jane = await axios.get('/users?username=Jane');
+      setJane(jane.data);
+      const derek = await axios.get('/users?username=Derek');
+      setDerek(derek.data);
+    };
+    fetchUser();
+  }, []);
 
   const itemStyle =
     'flex items-center gap-3 pl-2 py-2 hover:bg-gray-200 active:bg-gray-300 transition-all rounded-lg';
 
   return (
-    <nav className='hidden cursor-pointer w-[200px] xl:w-[290px] 2xl:w-[360px] lg:flex fixed left-2 overflow-y-scroll'>
-      <ul className='w-full'>
-        <li>
-          <a
-            onClick={() => navigate(`../profile/${user?.username}`)}
-            className={`${itemStyle}`}
-          >
-            {user?.profilePicture !== '' ? (
-              <img
-                src={user?.profilePicture}
-                alt=''
-                className='w-9 h-9 rounded-full object-cover'
-              />
-            ) : (
-              <div className='text-3xl font-black text-primary w-10 h-10 rounded-full bg-gray-light object-cover flex items-center justify-center'>
-                {user.username.charAt(0).toLocaleUpperCase()}
-              </div>
-            )}
-            <span>{user?.username}</span>
-          </a>
-        </li>
-        <li>
-          <a href='#' className={`${itemStyle}`}>
-            <i className='text-primary'>
-              <FriendsIcon
-                height='2.25em'
-                width='2.25em'
-                fill={'url(#grad1)'}
-              />
-            </i>
-            <span>Friends</span>
-          </a>
-        </li>
-        <li>
-          <a href='#' className={`${itemStyle}`}>
-            <i className='text-primary'>
-              <GroupsFillIcon height='2.25em' width='2.25em' />
-            </i>
-            <span>Groups</span>
-          </a>
-        </li>
-        <li>
-          <a href='#' className={`${itemStyle}`}>
-            <i className='text-primary'>
-              <MarketplaceFillIcon height='2.25em' width='2.25em' />
-            </i>
-            <span>Marketplace</span>
-          </a>
-        </li>
-        <li>
-          <a href='#' className={`${itemStyle}`}>
-            <i className='text-primary'>
-              <PlayFillIcon height='2.25em' width='2.25em' />
-            </i>
-            <span>Watch</span>
-          </a>
-        </li>
-        <li>
-          <a href='#' className={`${itemStyle}`}>
-            <i className='text-gray-dark'>
-              <ArrowDownIcon height='2.25em' width='2.25em' />
-            </i>
-            <span>See more</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className='hidden w-[200px] xl:w-[290px] 2xl:w-[360px] lg:flex lg:flex-col fixed left-2 overflow-y-scroll'>
+      <nav className='cursor-pointer w-full'>
+        <ul className='w-full'>
+          <li>
+            <a
+              onClick={() => navigate(`../profile/${user?.username}`)}
+              className={`${itemStyle}`}
+            >
+              {user?.profilePicture !== '' ? (
+                <img
+                  src={user?.profilePicture}
+                  alt=''
+                  className='w-9 h-9 rounded-full object-cover'
+                />
+              ) : (
+                <div className='text-3xl font-black text-primary w-9 h-9 rounded-full bg-gray-light object-cover flex items-center justify-center'>
+                  {user.username.charAt(0).toLocaleUpperCase()}
+                </div>
+              )}
+              <span>{user?.username}</span>
+            </a>
+          </li>
+          <li>
+            <a href='#' className={`${itemStyle}`}>
+              <i className='text-primary'>
+                <FriendsIcon
+                  height='2.25em'
+                  width='2.25em'
+                  fill={'url(#grad1)'}
+                />
+              </i>
+              <span>Friends</span>
+            </a>
+          </li>
+          <li>
+            <a href='#' className={`${itemStyle}`}>
+              <i className='text-primary'>
+                <GroupsFillIcon height='2.25em' width='2.25em' />
+              </i>
+              <span>Groups</span>
+            </a>
+          </li>
+          <li>
+            <a href='#' className={`${itemStyle}`}>
+              <i className='text-primary'>
+                <MarketplaceFillIcon height='2.25em' width='2.25em' />
+              </i>
+              <span>Marketplace</span>
+            </a>
+          </li>
+          <li>
+            <a href='#' className={`${itemStyle}`}>
+              <i className='text-primary'>
+                <PlayFillIcon height='2.25em' width='2.25em' />
+              </i>
+              <span>Watch</span>
+            </a>
+          </li>
+          <li>
+            <a href='#' className={`${itemStyle}`}>
+              <i className='text-gray-400'>
+                <ArrowDownIcon height='2.25em' width='2.25em' />
+              </i>
+              <span>See more</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className='h-[1px] px-4 w-full my-2 bg-gray-300'></div>
+
+      <div>
+        <h1 className='ml-2 mb-2 text-gray-dark font-bold text-lg'>
+          Suggested
+        </h1>
+        <nav className='cursor-pointer w-full'>
+          <ul className='w-full'>
+            <li>
+              <a
+                onClick={() => navigate(`../profile/${jane?.username}`)}
+                className={`${itemStyle}`}
+              >
+                {jane?.profilePicture !== '' ? (
+                  <img
+                    src={user?.profilePicture}
+                    alt=''
+                    className='w-9 h-9 rounded-full object-cover'
+                  />
+                ) : (
+                  <div className='text-3xl font-black text-primary w-9 h-9 rounded-full bg-gray-light object-cover flex items-center justify-center'>
+                    {jane.username.charAt(0).toLocaleUpperCase()}
+                  </div>
+                )}
+                <span>{jane?.username}</span>
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => navigate(`../profile/${derek?.username}`)}
+                className={`${itemStyle}`}
+              >
+                {derek?.profilePicture !== '' ? (
+                  <img
+                    src={derek?.profilePicture}
+                    alt=''
+                    className='w-9 h-9 rounded-full object-cover'
+                  />
+                ) : (
+                  <div className='text-3xl font-black text-primary w-9 h-9 rounded-full bg-gray-light object-cover flex items-center justify-center'>
+                    {derek.username.charAt(0).toLocaleUpperCase()}
+                  </div>
+                )}
+                <span>{derek?.username}</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
