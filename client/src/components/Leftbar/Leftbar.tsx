65e3728ce8b5a7b97d04845993733e5a3729f390
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 02:03:07 +0000

    Use user context and display default user image

--- client/src/components/Leftbar/Leftbar.tsx
+++ client/src/components/Leftbar/Leftbar.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 import { ArrowDownIcon } from '../Icons/ArrowDownIcon';
 import { FriendsIcon } from '../Icons/FriendsIcon';
 import { GroupsFillIcon } from '../Icons/GroupsFillIcon';
@@ -5,20 +8,33 @@ import { MarketplaceFillIcon } from '../Icons/MarketplaceFillIcon';
 import { PlayFillIcon } from '../Icons/PlayFillIcon';
 
 export const Leftbar = () => {
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
   const itemStyle =
     'flex items-center gap-3 pl-2 py-2 hover:bg-gray-200 active:bg-gray-300 transition-all rounded-lg';
 
   return (
-    <nav className='hidden w-[200px] xl:w-[290px] 2xl:w-[360px] lg:flex fixed left-2 overflow-y-scroll'>
+    <nav className='hidden cursor-pointer w-[200px] xl:w-[290px] 2xl:w-[360px] lg:flex fixed left-2 overflow-y-scroll'>
       <ul className='w-full'>
-        <li className=''>
-          <a href='#' className={`${itemStyle}`}>
-            <img
-              src=''
-              alt=''
-              className='bg-primary w-9 h-9 rounded-full object-cover'
-            />
-            <span>Chuck Norris</span>
+        <li>
+          <a
+            onClick={() => navigate(`../profile/${user?.username}`)}
+            className={`${itemStyle}`}
+          >
+            {user?.profilePicture !== '' ? (
+              <img
+                src={user?.profilePicture}
+                alt=''
+                className='w-9 h-9 rounded-full object-cover'
+              />
+            ) : (
+              <div className='text-3xl font-black text-primary w-10 h-10 rounded-full bg-gray-light object-cover flex items-center justify-center'>
+                {user.username.charAt(0).toLocaleUpperCase()}
+              </div>
+            )}
+            <span>{user?.username}</span>
           </a>
         </li>
         <li>
