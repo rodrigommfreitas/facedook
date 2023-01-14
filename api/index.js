45ff5a76245f86dce91efb3859998d4eae0db053
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 15:22:21 +0000

    Create image storage and upload method

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
@@ -21,11 +23,31 @@ mongoose
   .then(() => console.log('Database connected!'))
   .catch((err) => console.log(err));
 
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
 // middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  try {
+    return res.status(200).json('File uploded successfully');
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
