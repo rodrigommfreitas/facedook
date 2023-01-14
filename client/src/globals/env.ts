06dbd7054d76a49de2f0c3ea88cd6946e692f4cc
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 15:19:25 +0000

    Create global env variables

---
+++ client/src/globals/env.ts
@@ -0,0 +1,9 @@
+/* 
+   Due to an error while trying to use a.env file and process.env,
+  this file was created in order to store any env variables
+  as I wasn't willing to spend time trying to fix the other error.
+
+  Replace these variables according to your project settings and structure.
+*/
+
+export const PF = 'http://localhost:8800/images/';
