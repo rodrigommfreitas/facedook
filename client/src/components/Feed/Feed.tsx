9877c2a4921385e997d1a2341bea8fbdb1b1f453
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 01:57:50 +0000

    Make feed not show share post button on other users profiles

--- client/src/components/Feed/Feed.tsx
+++ client/src/components/Feed/Feed.tsx
@@ -1,14 +1,17 @@
 import { instance as axios } from '../../globals/axios';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Post } from '../Post/Post';
 import { Share } from '../Share/Share';
 import { PostType } from '../../globals/types';
+import { AuthContext } from '../../context/AuthContext';
 
 type Props = {
   username?: string;
 };
 
 export const Feed = ({ username }: Props) => {
+  const { user } = useContext(AuthContext);
+
   const [posts, setPosts] = useState<PostType[]>([]);
 
   useEffect(() => {
@@ -28,7 +31,7 @@ export const Feed = ({ username }: Props) => {
         'h-screen w-[640px] md:w-2/3 lg:w-[572px] xl:w-[652px] sm:mx-auto md:mx-8 lg:mx-auto'
       } `}
     >
-      <Share />
+      {(username === user?.username || !username) && <Share />}
       {posts.map((post, i) => (
         <Post key={i} post={post} />
       ))}
