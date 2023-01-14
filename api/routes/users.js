7277b25fd32cd56b74af4e80f208991fd4dc4efe
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-14 23:14:55 +0000

    Get user's friends route

--- api/routes/users.js
+++ api/routes/users.js
@@ -59,6 +59,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Get user's friends
+router.get('/friends/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friends = await Promise.all(
+      user.following.map((friendId) => {
+        return User.findById(friendId);
+      })
+    );
+    let friendList = [];
+    friends.map((friend) => {
+      const { _id, username, profilePicture } = friend;
+      friendList.push({ _id, username, profilePicture });
+    });
+    res.status(200).json(friendList);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Follow user
 router.put('/:id/follow', async (req, res) => {
   if (req.body.userId !== req.params.id) {
